8eb50dcf0da2d7a49e888e4c95822c66f6379fc6
JoelCabrera-irnma <140646912+JoelCabrera-irnma@users.noreply.github.com>
2023-11-03 18:00:54 -0300

    Added input Checkbox

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 
-
-const myLibrary = [];
-
-function Book(nombre, autor, pages) {
-  this.nombre = nombre
-  this.autor = autor
-  this.pages = pages
-}
-
-function addBookToLibrary() {
-    const nombre = document.getElementById('nombre').value
-    // const autor =
-    // const pages =
-   
-    const newBook = new Book(nombre, autor, pages)
-    
-    myLibrary.push(newBook)
-
-    createCard()
-}
-
 const mostrarDialogButton = document.getElementById("mostrarDialog");
 const cerrarDialogButton = document.getElementById("cerrarDialog");
 const miDialog = document.getElementById("miDialog");
@@ -40,15 +19,41 @@ let getInfo = document.querySelector('#myForm').addEventListener('submit',functi
   })
 
 
+const myLibrary = [];
+
+function Book(nombre, autor, pages, leido) {
+  this.nombre = nombre
+  this.autor = autor
+  this.pages = pages
+  this.leido = leido 
+}
+
+function addBookToLibrary() {
+    const nombre = document.getElementById('nombre').value
+    const autor = document.getElementById('autor').value
+    const pages = document.getElementById('pages').value
+    const leido = document.getElementById('leido').checked
+   
+    const newBook = new Book(nombre, autor, pages, leido)
+    console.log(leido)
+    myLibrary.push(newBook)
+
+    createCard()
+}
+
 function createCard(){
     const cardsContenedor = document.querySelector('.cardsContenedor')
     cardsContenedor.innerHTML = ""
     for (let i = 0; i < myLibrary.length; i++) {
         const card = document.createElement('div')
         card.className = 'card'
-        card.innerHTML = `<p>libro: ${myLibrary[i].nombre}</p>`
-                
+        card.innerHTML = `
+                        <p>libro: ${myLibrary[i].nombre}</p>
+                        <p>autor: ${myLibrary[i].autor}</p>
+                        <p>paginas: ${myLibrary[i].pages}</p>
+                        <p>leido: ${myLibrary[i].leido ? "Ya leido" : "No leido"}</p>
+        `  
         cardsContenedor.appendChild(card)
-    }
-    
+        console.log(myLibrary)
+    } 
 }
