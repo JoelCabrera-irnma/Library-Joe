474f94451c723ce8e10d9752f678ea48d5915c1e
JoelCabrera-irnma <140646912+JoelCabrera-irnma@users.noreply.github.com>
2023-11-06 11:00:18 -0300

    Add style Css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const mostrarDialogButton = document.getElementById("mostrarDialog");
-const cerrarDialogButton = document.getElementById("cerrarDialog");
+const enviarBookButton = document.getElementById("enviarBook");
 const miDialog = document.getElementById("miDialog");
 
 // Mostrar el diálogo al hacer clic en el botón "Mostrar Diálogo"
@@ -9,7 +9,7 @@ mostrarDialogButton.addEventListener("click", function() {
 });
 
 // Cerrar el diálogo al hacer clic en el botón "Cerrar"
-cerrarDialogButton.addEventListener("click", function() {
+enviarBookButton.addEventListener("click", function() {
     miDialog.close();
 });
 
@@ -20,20 +20,22 @@ let getInfo = document.querySelector('#myForm').addEventListener('submit',functi
 
 const myLibrary = [];
 
-function Book(nombre, autor, pages, leido) {
+function Book(nombre, autor, pages, calificacion, leido) {
   this.nombre = nombre
   this.autor = autor
   this.pages = pages
-  this.leido = leido 
+  this.leido = leido
+  this.calificacion = calificacion 
 }
 
 function addBookToLibrary() {
     const nombre = document.getElementById('nombre').value
     const autor = document.getElementById('autor').value
     const pages = document.getElementById('pages').value
+    const calificacion = document.getElementById('calificacion').value
     const leido = document.getElementById('leido').checked
-   
-    const newBook = new Book(nombre, autor, pages, leido)
+    
+    const newBook = new Book(nombre, autor, pages, calificacion, leido,)
 
     myLibrary.push(newBook)
 
@@ -48,19 +50,14 @@ function createCard(){
         const card = document.createElement('div')
         card.className = 'card'
         card.innerHTML = `
-                        <p>libro: ${myLibrary[i].nombre}</p>
-                        <p>autor: ${myLibrary[i].autor}</p>
-                        <p>paginas: ${myLibrary[i].pages}</p>
-                        <p>leido: ${myLibrary[i].leido ? "Ya leido" : "No leido"}</p>
+                        <p class="encabezado"> Titulo ${i+1}:</p>
+                        <p class="titleCard"> ${myLibrary[i].nombre}</p>
+                        <p class="dato"> Autor: ${myLibrary[i].autor}</p>
+                        <p class="dato"> Paginas: ${myLibrary[i].pages}</p>
+                        <p class="dato"> Calficacion: ${myLibrary[i].calificacion} ★</p>
         `  
         //console.log(i)
-        cardsContenedor.appendChild(card)
-        const butonDelete = document.createElement('button')
-        butonDelete.textContent = "Delete"
-        butonDelete.setAttribute('onclick',`removeElement(${i})`)
-        card.appendChild(butonDelete)
-
-        myLibrary[i].leido ? card.className = 'green' : card.className = 'red';
+        myLibrary[i].leido ? card.classList.add('green') : card.classList.add('red');
 
         const butonStatusRead = document.createElement('button')
         myLibrary[i].leido ? butonStatusRead.textContent = "Read" : butonStatusRead.textContent = "No Read";
@@ -68,9 +65,19 @@ function createCard(){
         butonStatusRead.addEventListener('click',function () {
             if(card.classList.contains("green")){
                 card.classList.replace("green", "red")
+                butonStatusRead.textContent = "No Read"
             }
-            else{card.classList.replace("red", "green")}
+            else{
+                card.classList.replace("red", "green");
+                butonStatusRead.textContent = "Read"
+            }
         })
+
+        cardsContenedor.appendChild(card)
+        const butonDelete = document.createElement('button')
+        butonDelete.textContent = "Delete"
+        butonDelete.setAttribute('onclick',`removeElement(${i})`)
+        card.appendChild(butonDelete)
     } 
 }
 
